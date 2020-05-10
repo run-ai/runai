fe869fcf55b40fcc18b5ca7089416bab7bb483ba
levosos <razhaleva@gmail.com>
2020-05-10 11:22:51 +0300

    Default arguments in runai.utils.random.number()
    - Added UTs

--- runai/utils/random.py
+++ runai/utils/random.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import # to support 'import random' in Python 2
 import random as r
 import string as s
 
-number = r.randint
+number = lambda a=10, b=100: r.randint(a, b)
 
 def string(length=5):
     return ''.join(r.choice(s.ascii_letters + s.digits + s.punctuation) for _ in range(length))
